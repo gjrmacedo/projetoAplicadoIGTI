87f991ff78dc99692123c25148100192ab432059
Gerson Macedo Junior <gjrmacedo@gmail.com>
2019-02-03 22:03:32 -0200

    BKP

---
+++ Empregando.Relatorios.API/Controllers/VagasController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Empregando.Relatorios.API.Controllers
+{
+    public class VagasController : ApiController
+    {
+        // GET: Vagas
+        [HttpGet]
+        [Route("vagas")]
+        public IHttpActionResult Get()
+        {
+            return Ok(
+                new List<string>
+                {
+                    "Vaga1",
+                    "Vaga2",
+                    "Vaga3",
+                    "Vaga4",
+                    "Vaga4"
+                }
+            );
+        }
+    }
+}
---
+++ Empregando.Relatorios.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Empregando.Relatorios.API.WebApiApplication" Language="C#" %>
